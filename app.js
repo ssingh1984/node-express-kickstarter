8fd4b63868872756e6efc173b261266ce639ff8a
ghacc4me <mailme.ss84@gmail.com>
2018-11-02 03:48:27 +0530

    some changes

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ require('./routes/index')(app, express);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+});   
 
 
 // error handler
